1873332a06f22edffe2f864efca4ff52a5cfb37a
Aaron <69317200+Aaronphilip2003@users.noreply.github.com>
2021-10-12 12:34:37 +0530

    Create Q33.py
    Added Q33 which is a Fibonacci sequence. To make the pattern more visible, I did 2*n.

---
+++ newfolder/Q33.py
@@ -0,0 +1,23 @@
+def fibonacci_pattern(n):
+	if n<=1:
+		return n
+	else:
+		return (fibonacci_pattern(n-1)+fibonacci_pattern(n-2))
+
+
+n=int(input())
+for i in range(0,2*n):
+	for j in range(fibonacci_pattern(i)):
+		print("*",end="")
+	print()
+
+  
+# *
+# *
+# **
+# ***
+# *****
+# ********
+# *************
+# *********************
+# **********************************
